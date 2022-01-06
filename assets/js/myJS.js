357130a42d8d31cd7987d32943695caadc319f9e
marselezashop <marseleza.etsy@bk.ru>
2022-01-06 08:14:46 +0700

    lang

--- assets/js/myJS.js
+++ assets/js/myJS.js
@@ -1,5 +1,10 @@
 function select_language(language) {
     $("[lang]").each(function () {
+    	if ( $( this ).is( "#skip" ))
+    	{
+    		return;
+			//$(this).attr("lang", language);
+    	} 
         if ($(this).attr("lang") == language)
             $(this).show();
         else
